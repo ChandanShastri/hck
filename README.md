020a9738d218601a84d79a6a4a5c076e270e6e76
Chandan Shastri <crshastri@gmail.com>
2018-09-28 22:11:58 +0530

    A

--- README.md
+++
@@ -1,4 +0,0 @@
-# hck
-Chandan Shastri   | crshastri@gmail.com
-
-This is the Main CodeBase of my Internship Project for HackerEarth.
--- js/Sc.js
+++
@@ -1,8 +0,0 @@
-function cleanup() {
-  document.getElementById('bodyplace').innerHTML="<br><center>Sorry, you are not allowed to do this.</center>";
-};
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-124730361-1');
